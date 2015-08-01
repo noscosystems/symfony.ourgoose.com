7db01c9b32ce26bc123dd6b700b9dd132212c329
Zander Baldwin <hello@zanderbaldwin.com>
2015-08-01 12:45:59 +0100

    Changes by Steven

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,11 +3,20 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+
+    public function indexAction()
     {
-        return $this->render('AppBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    public function numberAction()
+    {
+    	$number = rand(0,100);
+
+            return $this->render('AppBundle:Default:home.html.twig', array('number' => $number));
     }
 }
